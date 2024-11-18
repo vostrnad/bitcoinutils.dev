168e4061e48e0998078263113a0750afa59a46af
Vojtěch Strnad <43024885+vostrnad@users.noreply.github.com>
2024-11-18 18:52:18 +0100

    Define all opcodes in opcodes.ts

--- src/lib/utils/bitcoin/script/interpreter.ts
+++ src/lib/utils/bitcoin/script/interpreter.ts
@@ -16,6 +16,113 @@ import {
   isXOnlyKey,
 } from '$lib/utils/bitcoin/pubkey'
 import {
+  OP_0,
+  OP_0NOTEQUAL,
+  OP_1,
+  OP_10,
+  OP_11,
+  OP_12,
+  OP_13,
+  OP_14,
+  OP_15,
+  OP_16,
+  OP_1ADD,
+  OP_1NEGATE,
+  OP_1SUB,
+  OP_2,
+  OP_2DIV,
+  OP_2DROP,
+  OP_2DUP,
+  OP_2MUL,
+  OP_2OVER,
+  OP_2ROT,
+  OP_2SWAP,
+  OP_3,
+  OP_3DUP,
+  OP_4,
+  OP_5,
+  OP_6,
+  OP_7,
+  OP_8,
+  OP_9,
+  OP_ABS,
+  OP_ADD,
+  OP_AND,
+  OP_BOOLAND,
+  OP_BOOLOR,
+  OP_CAT,
+  OP_CHECKLOCKTIMEVERIFY,
+  OP_CHECKMULTISIG,
+  OP_CHECKMULTISIGVERIFY,
+  OP_CHECKSEQUENCEVERIFY,
+  OP_CHECKSIG,
+  OP_CHECKSIGADD,
+  OP_CHECKSIGVERIFY,
+  OP_CODESEPARATOR,
+  OP_DEPTH,
+  OP_DIV,
+  OP_DROP,
+  OP_DUP,
+  OP_ELSE,
+  OP_ENDIF,
+  OP_EQUAL,
+  OP_EQUALVERIFY,
+  OP_FROMALTSTACK,
+  OP_GREATERTHAN,
+  OP_GREATERTHANOREQUAL,
+  OP_HASH160,
+  OP_HASH256,
+  OP_IF,
+  OP_IFDUP,
+  OP_INVERT,
+  OP_LEFT,
+  OP_LESSTHAN,
+  OP_LESSTHANOREQUAL,
+  OP_LSHIFT,
+  OP_MAX,
+  OP_MIN,
+  OP_MOD,
+  OP_MUL,
+  OP_NEGATE,
+  OP_NIP,
+  OP_NOP,
+  OP_NOP1,
+  OP_NOP10,
+  OP_NOP4,
+  OP_NOP5,
+  OP_NOP6,
+  OP_NOP7,
+  OP_NOP8,
+  OP_NOP9,
+  OP_NOT,
+  OP_NOTIF,
+  OP_NUMEQUAL,
+  OP_NUMEQUALVERIFY,
+  OP_NUMNOTEQUAL,
+  OP_OR,
+  OP_OVER,
+  OP_PICK,
+  OP_PUSHDATA1,
+  OP_PUSHDATA4,
+  OP_RETURN,
+  OP_RIGHT,
+  OP_RIPEMD160,
+  OP_ROLL,
+  OP_ROT,
+  OP_RSHIFT,
+  OP_SHA1,
+  OP_SHA256,
+  OP_SIZE,
+  OP_SUB,
+  OP_SUBSTR,
+  OP_SWAP,
+  OP_TOALTSTACK,
+  OP_TUCK,
+  OP_VERIFY,
+  OP_WITHIN,
+  OP_XOR,
+} from '$lib/utils/bitcoin/script/opcodes'
+import {
   isAnyKnownSignature,
   isValidDERSignature,
   isValidSchnorrSignature,
@@ -29,141 +136,6 @@ import {
   intLEToBytes,
   uint8ArrayEqual,
 } from '$lib/utils/uintarray'
-
-const OP_0 = 0x00
-// const OP_FALSE = OP_0
-const OP_PUSHDATA1 = 0x4c
-// const OP_PUSHDATA2 = 0x4d
-const OP_PUSHDATA4 = 0x4e
-const OP_1NEGATE = 0x4f
-// const OP_RESERVED = 0x50
-const OP_1 = 0x51
-// const OP_TRUE = OP_1
-const OP_2 = 0x52
-const OP_3 = 0x53
-const OP_4 = 0x54
-const OP_5 = 0x55
-const OP_6 = 0x56
-const OP_7 = 0x57
-const OP_8 = 0x58
-const OP_9 = 0x59
-const OP_10 = 0x5a
-const OP_11 = 0x5b
-const OP_12 = 0x5c
-const OP_13 = 0x5d
-const OP_14 = 0x5e
-const OP_15 = 0x5f
-const OP_16 = 0x60
-
-// control
-const OP_NOP = 0x61
-// const OP_VER = 0x62
-const OP_IF = 0x63
-const OP_NOTIF = 0x64
-// const OP_VERIF = 0x65
-// const OP_VERNOTIF = 0x66
-const OP_ELSE = 0x67
-const OP_ENDIF = 0x68
-const OP_VERIFY = 0x69
-const OP_RETURN = 0x6a
-
-// stack ops
-const OP_TOALTSTACK = 0x6b
-const OP_FROMALTSTACK = 0x6c
-const OP_2DROP = 0x6d
-const OP_2DUP = 0x6e
-const OP_3DUP = 0x6f
-const OP_2OVER = 0x70
-const OP_2ROT = 0x71
-const OP_2SWAP = 0x72
-const OP_IFDUP = 0x73
-const OP_DEPTH = 0x74
-const OP_DROP = 0x75
-const OP_DUP = 0x76
-const OP_NIP = 0x77
-const OP_OVER = 0x78
-const OP_PICK = 0x79
-const OP_ROLL = 0x7a
-const OP_ROT = 0x7b
-const OP_SWAP = 0x7c
-const OP_TUCK = 0x7d
-
-// splice ops
-const OP_CAT = 0x7e
-const OP_SUBSTR = 0x7f
-const OP_LEFT = 0x80
-const OP_RIGHT = 0x81
-const OP_SIZE = 0x82
-
-// bit logic
-const OP_INVERT = 0x83
-const OP_AND = 0x84
-const OP_OR = 0x85
-const OP_XOR = 0x86
-const OP_EQUAL = 0x87
-const OP_EQUALVERIFY = 0x88
-// const OP_RESERVED1 = 0x89
-// const OP_RESERVED2 = 0x8a
-
-// numeric
-const OP_1ADD = 0x8b
-const OP_1SUB = 0x8c
-const OP_2MUL = 0x8d
-const OP_2DIV = 0x8e
-const OP_NEGATE = 0x8f
-const OP_ABS = 0x90
-const OP_NOT = 0x91
-const OP_0NOTEQUAL = 0x92
-
-const OP_ADD = 0x93
-const OP_SUB = 0x94
-const OP_MUL = 0x95
-const OP_DIV = 0x96
-const OP_MOD = 0x97
-const OP_LSHIFT = 0x98
-const OP_RSHIFT = 0x99
-
-const OP_BOOLAND = 0x9a
-const OP_BOOLOR = 0x9b
-const OP_NUMEQUAL = 0x9c
-const OP_NUMEQUALVERIFY = 0x9d
-const OP_NUMNOTEQUAL = 0x9e
-const OP_LESSTHAN = 0x9f
-const OP_GREATERTHAN = 0xa0
-const OP_LESSTHANOREQUAL = 0xa1
-const OP_GREATERTHANOREQUAL = 0xa2
-const OP_MIN = 0xa3
-const OP_MAX = 0xa4
-const OP_WITHIN = 0xa5
-
-// crypto
-const OP_RIPEMD160 = 0xa6
-const OP_SHA1 = 0xa7
-const OP_SHA256 = 0xa8
-const OP_HASH160 = 0xa9
-const OP_HASH256 = 0xaa
-const OP_CODESEPARATOR = 0xab
-const OP_CHECKSIG = 0xac
-const OP_CHECKSIGVERIFY = 0xad
-const OP_CHECKMULTISIG = 0xae
-const OP_CHECKMULTISIGVERIFY = 0xaf
-
-// expansion
-const OP_NOP1 = 0xb0
-const OP_CHECKLOCKTIMEVERIFY = 0xb1
-// const OP_NOP2 = OP_CHECKLOCKTIMEVERIFY
-const OP_CHECKSEQUENCEVERIFY = 0xb2
-// const OP_NOP3 = OP_CHECKSEQUENCEVERIFY
-const OP_NOP4 = 0xb3
-const OP_NOP5 = 0xb4
-const OP_NOP6 = 0xb5
-const OP_NOP7 = 0xb6
-const OP_NOP8 = 0xb7
-const OP_NOP9 = 0xb8
-const OP_NOP10 = 0xb9
-
-// Opcode added by BIP 342 (Tapscript)
-const OP_CHECKSIGADD = 0xba
 
 const MAX_PUBKEYS_PER_MULTISIG = 20
 
--- src/lib/utils/bitcoin/script/opcodes.ts
+++ src/lib/utils/bitcoin/script/opcodes.ts
@@ -1,143 +1,270 @@
 import { createArray } from '$lib/utils/array'
 
+// push value
 export const OP_0 = 0x00
-
+// export const OP_FALSE = OP_0
 export const OP_PUSHBYTES_1 = 0x01
 export const OP_PUSHBYTES_75 = 0x4b
-
 export const OP_PUSHDATA1 = 0x4c
 export const OP_PUSHDATA2 = 0x4d
 export const OP_PUSHDATA4 = 0x4e
-
+export const OP_1NEGATE = 0x4f
+export const OP_RESERVED = 0x50
 export const OP_1 = 0x51
+// export const OP_TRUE = OP_1
+export const OP_2 = 0x52
+export const OP_3 = 0x53
+export const OP_4 = 0x54
+export const OP_5 = 0x55
+export const OP_6 = 0x56
+export const OP_7 = 0x57
+export const OP_8 = 0x58
+export const OP_9 = 0x59
+export const OP_10 = 0x5a
+export const OP_11 = 0x5b
+export const OP_12 = 0x5c
+export const OP_13 = 0x5d
+export const OP_14 = 0x5e
+export const OP_15 = 0x5f
 export const OP_16 = 0x60
 
-export const OP_1NEGATE = 0x4f
+// control
+export const OP_NOP = 0x61
+export const OP_VER = 0x62
+export const OP_IF = 0x63
+export const OP_NOTIF = 0x64
+export const OP_VERIF = 0x65
+export const OP_VERNOTIF = 0x66
+export const OP_ELSE = 0x67
+export const OP_ENDIF = 0x68
+export const OP_VERIFY = 0x69
+export const OP_RETURN = 0x6a
+
+// stack ops
+export const OP_TOALTSTACK = 0x6b
+export const OP_FROMALTSTACK = 0x6c
+export const OP_2DROP = 0x6d
+export const OP_2DUP = 0x6e
+export const OP_3DUP = 0x6f
+export const OP_2OVER = 0x70
+export const OP_2ROT = 0x71
+export const OP_2SWAP = 0x72
+export const OP_IFDUP = 0x73
+export const OP_DEPTH = 0x74
+export const OP_DROP = 0x75
+export const OP_DUP = 0x76
+export const OP_NIP = 0x77
+export const OP_OVER = 0x78
+export const OP_PICK = 0x79
+export const OP_ROLL = 0x7a
+export const OP_ROT = 0x7b
+export const OP_SWAP = 0x7c
+export const OP_TUCK = 0x7d
+
+// splice ops
+export const OP_CAT = 0x7e
+export const OP_SUBSTR = 0x7f
+export const OP_LEFT = 0x80
+export const OP_RIGHT = 0x81
+export const OP_SIZE = 0x82
+
+// bit logic
+export const OP_INVERT = 0x83
+export const OP_AND = 0x84
+export const OP_OR = 0x85
+export const OP_XOR = 0x86
+export const OP_EQUAL = 0x87
+export const OP_EQUALVERIFY = 0x88
+export const OP_RESERVED1 = 0x89
+export const OP_RESERVED2 = 0x8a
+
+// numeric
+export const OP_1ADD = 0x8b
+export const OP_1SUB = 0x8c
+export const OP_2MUL = 0x8d
+export const OP_2DIV = 0x8e
+export const OP_NEGATE = 0x8f
+export const OP_ABS = 0x90
+export const OP_NOT = 0x91
+export const OP_0NOTEQUAL = 0x92
+
+export const OP_ADD = 0x93
+export const OP_SUB = 0x94
+export const OP_MUL = 0x95
+export const OP_DIV = 0x96
+export const OP_MOD = 0x97
+export const OP_LSHIFT = 0x98
+export const OP_RSHIFT = 0x99
+
+export const OP_BOOLAND = 0x9a
+export const OP_BOOLOR = 0x9b
+export const OP_NUMEQUAL = 0x9c
+export const OP_NUMEQUALVERIFY = 0x9d
+export const OP_NUMNOTEQUAL = 0x9e
+export const OP_LESSTHAN = 0x9f
+export const OP_GREATERTHAN = 0xa0
+export const OP_LESSTHANOREQUAL = 0xa1
+export const OP_GREATERTHANOREQUAL = 0xa2
+export const OP_MIN = 0xa3
+export const OP_MAX = 0xa4
+
+export const OP_WITHIN = 0xa5
+
+// crypto
+export const OP_RIPEMD160 = 0xa6
+export const OP_SHA1 = 0xa7
+export const OP_SHA256 = 0xa8
+export const OP_HASH160 = 0xa9
+export const OP_HASH256 = 0xaa
+export const OP_CODESEPARATOR = 0xab
+export const OP_CHECKSIG = 0xac
+export const OP_CHECKSIGVERIFY = 0xad
+export const OP_CHECKMULTISIG = 0xae
+export const OP_CHECKMULTISIGVERIFY = 0xaf
+
+// expansion
+export const OP_NOP1 = 0xb0
+export const OP_CHECKLOCKTIMEVERIFY = 0xb1
+// export const OP_NOP2 = OP_CHECKLOCKTIMEVERIFY
+export const OP_CHECKSEQUENCEVERIFY = 0xb2
+// export const OP_NOP3 = OP_CHECKSEQUENCEVERIFY
+export const OP_NOP4 = 0xb3
+export const OP_NOP5 = 0xb4
+export const OP_NOP6 = 0xb5
+export const OP_NOP7 = 0xb6
+export const OP_NOP8 = 0xb7
+export const OP_NOP9 = 0xb8
+export const OP_NOP10 = 0xb9
+
+// Opcode added by BIP 342 (Tapscript)
+export const OP_CHECKSIGADD = 0xba
+
+export const OP_INVALIDOPCODE = 0xff
 
 const opcodesShort: Array<[string, number | string]> = [
   // push value
-  ['0', 0x00],
+  ['0', OP_0],
   ['FALSE', '0'],
-  ['1NEGATE', 0x4f],
-  ['RESERVED', 0x50],
+  ['1NEGATE', OP_1NEGATE],
+  ['RESERVED', OP_RESERVED],
   ...createArray<[string, number]>(16, (i) => [`${i + 1}`, i + OP_1]),
   ['TRUE', '1'],
   ...createArray<[string, string]>(16, (i) => [`PUSHNUM_${i + 1}`, `${i + 1}`]),
 
   // control
-  ['NOP', 0x61],
-  ['VER', 0x62],
-  ['IF', 0x63],
-  ['NOTIF', 0x64],
-  ['VERIF', 0x65],
-  ['VERNOTIF', 0x66],
-  ['ELSE', 0x67],
-  ['ENDIF', 0x68],
-  ['VERIFY', 0x69],
-  ['RETURN', 0x6a],
+  ['NOP', OP_NOP],
+  ['VER', OP_VER],
+  ['IF', OP_IF],
+  ['NOTIF', OP_NOTIF],
+  ['VERIF', OP_VERIF],
+  ['VERNOTIF', OP_VERNOTIF],
+  ['ELSE', OP_ELSE],
+  ['ENDIF', OP_ENDIF],
+  ['VERIFY', OP_VERIFY],
+  ['RETURN', OP_RETURN],
 
   // stack ops
-  ['TOALTSTACK', 0x6b],
-  ['FROMALTSTACK', 0x6c],
-  ['2DROP', 0x6d],
-  ['2DUP', 0x6e],
-  ['3DUP', 0x6f],
-  ['2OVER', 0x70],
-  ['2ROT', 0x71],
-  ['2SWAP', 0x72],
-  ['IFDUP', 0x73],
-  ['DEPTH', 0x74],
-  ['DROP', 0x75],
-  ['DUP', 0x76],
-  ['NIP', 0x77],
-  ['OVER', 0x78],
-  ['PICK', 0x79],
-  ['ROLL', 0x7a],
-  ['ROT', 0x7b],
-  ['SWAP', 0x7c],
-  ['TUCK', 0x7d],
+  ['TOALTSTACK', OP_TOALTSTACK],
+  ['FROMALTSTACK', OP_FROMALTSTACK],
+  ['2DROP', OP_2DROP],
+  ['2DUP', OP_2DUP],
+  ['3DUP', OP_3DUP],
+  ['2OVER', OP_2OVER],
+  ['2ROT', OP_2ROT],
+  ['2SWAP', OP_2SWAP],
+  ['IFDUP', OP_IFDUP],
+  ['DEPTH', OP_DEPTH],
+  ['DROP', OP_DROP],
+  ['DUP', OP_DUP],
+  ['NIP', OP_NIP],
+  ['OVER', OP_OVER],
+  ['PICK', OP_PICK],
+  ['ROLL', OP_ROLL],
+  ['ROT', OP_ROT],
+  ['SWAP', OP_SWAP],
+  ['TUCK', OP_TUCK],
 
   // splice ops
-  ['CAT', 0x7e],
-  ['SUBSTR', 0x7f],
-  ['LEFT', 0x80],
-  ['RIGHT', 0x81],
-  ['SIZE', 0x82],
+  ['CAT', OP_CAT],
+  ['SUBSTR', OP_SUBSTR],
+  ['LEFT', OP_LEFT],
+  ['RIGHT', OP_RIGHT],
+  ['SIZE', OP_SIZE],
 
   // bit logic
-  ['INVERT', 0x83],
-  ['AND', 0x84],
-  ['OR', 0x85],
-  ['XOR', 0x86],
-  ['EQUAL', 0x87],
-  ['EQUALVERIFY', 0x88],
-  ['RESERVED1', 0x89],
-  ['RESERVED2', 0x8a],
+  ['INVERT', OP_INVERT],
+  ['AND', OP_AND],
+  ['OR', OP_OR],
+  ['XOR', OP_XOR],
+  ['EQUAL', OP_EQUAL],
+  ['EQUALVERIFY', OP_EQUALVERIFY],
+  ['RESERVED1', OP_RESERVED1],
+  ['RESERVED2', OP_RESERVED2],
 
   // numeric
-  ['1ADD', 0x8b],
-  ['1SUB', 0x8c],
-  ['2MUL', 0x8d],
-  ['2DIV', 0x8e],
-  ['NEGATE', 0x8f],
-  ['ABS', 0x90],
-  ['NOT', 0x91],
-  ['0NOTEQUAL', 0x92],
+  ['1ADD', OP_1ADD],
+  ['1SUB', OP_1SUB],
+  ['2MUL', OP_2MUL],
+  ['2DIV', OP_2DIV],
+  ['NEGATE', OP_NEGATE],
+  ['ABS', OP_ABS],
+  ['NOT', OP_NOT],
+  ['0NOTEQUAL', OP_0NOTEQUAL],
 
-  ['ADD', 0x93],
-  ['SUB', 0x94],
-  ['MUL', 0x95],
-  ['DIV', 0x96],
-  ['MOD', 0x97],
-  ['LSHIFT', 0x98],
-  ['RSHIFT', 0x99],
+  ['ADD', OP_ADD],
+  ['SUB', OP_SUB],
+  ['MUL', OP_MUL],
+  ['DIV', OP_DIV],
+  ['MOD', OP_MOD],
+  ['LSHIFT', OP_LSHIFT],
+  ['RSHIFT', OP_RSHIFT],
 
-  ['BOOLAND', 0x9a],
-  ['BOOLOR', 0x9b],
-  ['NUMEQUAL', 0x9c],
-  ['NUMEQUALVERIFY', 0x9d],
-  ['NUMNOTEQUAL', 0x9e],
-  ['LESSTHAN', 0x9f],
-  ['GREATERTHAN', 0xa0],
-  ['LESSTHANOREQUAL', 0xa1],
-  ['GREATERTHANOREQUAL', 0xa2],
-  ['MIN', 0xa3],
-  ['MAX', 0xa4],
+  ['BOOLAND', OP_BOOLAND],
+  ['BOOLOR', OP_BOOLOR],
+  ['NUMEQUAL', OP_NUMEQUAL],
+  ['NUMEQUALVERIFY', OP_NUMEQUALVERIFY],
+  ['NUMNOTEQUAL', OP_NUMNOTEQUAL],
+  ['LESSTHAN', OP_LESSTHAN],
+  ['GREATERTHAN', OP_GREATERTHAN],
+  ['LESSTHANOREQUAL', OP_LESSTHANOREQUAL],
+  ['GREATERTHANOREQUAL', OP_GREATERTHANOREQUAL],
+  ['MIN', OP_MIN],
+  ['MAX', OP_MAX],
 
-  ['WITHIN', 0xa5],
+  ['WITHIN', OP_WITHIN],
 
   // crypto
-  ['RIPEMD160', 0xa6],
-  ['SHA1', 0xa7],
-  ['SHA256', 0xa8],
-  ['HASH160', 0xa9],
-  ['HASH256', 0xaa],
-  ['CODESEPARATOR', 0xab],
-  ['CHECKSIG', 0xac],
-  ['CHECKSIGVERIFY', 0xad],
-  ['CHECKMULTISIG', 0xae],
-  ['CHECKMULTISIGVERIFY', 0xaf],
+  ['RIPEMD160', OP_RIPEMD160],
+  ['SHA1', OP_SHA1],
+  ['SHA256', OP_SHA256],
+  ['HASH160', OP_HASH160],
+  ['HASH256', OP_HASH256],
+  ['CODESEPARATOR', OP_CODESEPARATOR],
+  ['CHECKSIG', OP_CHECKSIG],
+  ['CHECKSIGVERIFY', OP_CHECKSIGVERIFY],
+  ['CHECKMULTISIG', OP_CHECKMULTISIG],
+  ['CHECKMULTISIGVERIFY', OP_CHECKMULTISIGVERIFY],
 
   // expansion
-  ['NOP1', 0xb0],
-  ['CHECKLOCKTIMEVERIFY', 0xb1],
+  ['NOP1', OP_NOP1],
+  ['CHECKLOCKTIMEVERIFY', OP_CHECKLOCKTIMEVERIFY],
   ['CLTV', 'CHECKLOCKTIMEVERIFY'],
   ['NOP2', 'CHECKLOCKTIMEVERIFY'],
-  ['CHECKSEQUENCEVERIFY', 0xb2],
+  ['CHECKSEQUENCEVERIFY', OP_CHECKSEQUENCEVERIFY],
   ['CSV', 'CHECKSEQUENCEVERIFY'],
   ['NOP3', 'CHECKSEQUENCEVERIFY'],
-  ['NOP4', 0xb3],
-  ['NOP5', 0xb4],
-  ['NOP6', 0xb5],
-  ['NOP7', 0xb6],
-  ['NOP8', 0xb7],
-  ['NOP9', 0xb8],
-  ['NOP10', 0xb9],
+  ['NOP4', OP_NOP4],
+  ['NOP5', OP_NOP5],
+  ['NOP6', OP_NOP6],
+  ['NOP7', OP_NOP7],
+  ['NOP8', OP_NOP8],
+  ['NOP9', OP_NOP9],
+  ['NOP10', OP_NOP10],
 
   // Opcode added by BIP 342 (Tapscript)
-  ['CHECKSIGADD', 0xba],
+  ['CHECKSIGADD', OP_CHECKSIGADD],
 
-  ['INVALIDOPCODE', 0xff],
+  ['INVALIDOPCODE', OP_INVALIDOPCODE],
 ]
 
 const unshorten = (opcode: string) => `OP_${opcode}`
